0a38e7acf8e3d6aa0bb07c859cda7812a770f7a9
Timm638 <Timm638@gmail.com>
2024-01-27 14:49:49 +0100

    adapted code to work without particles

--- Scripts/BodyPartObject.gd
+++ Scripts/BodyPartObject.gd
@@ -6,18 +6,11 @@ signal released(this : RigidBody2D)
 @export var sprite : Sprite2D
 @export var particleSystem1 : GPUParticles2D
 @export var particleSystem2 : GPUParticles2D
-
-var dock_points : Array[Area2D]
-var dock_points_contact : Array[PairArea2D]
-var offset_held = Vector2(0, 0);
 @export var rotation_sensitivity : float = 0.15
 
 var dock_points : Array[Area2D]
 var dock_points_contact : Array[PairArea2D]
-var offset_held = Vector2(0, 0)
-=======
-#var tempPosition = Vect
->>>>>>> origin/dev_markus
+var offset_held = Vector2(0, 0);
 
 var held = false : 
 	get:
@@ -41,14 +34,6 @@ var held = false :
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
-
-
-	#else:
-		#particleSystem1.emitting = false
-	#sprite = $HeadTest
-	pass # Replace with function body.
-
 	sprite = get_child(0)
 	for child in get_children():
 		if child.is_in_group("DockPoint"):
@@ -74,15 +59,15 @@ func append_to_dock(own_child : Area2D, dock_point : Area2D):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(linear_velocity)
-	if linear_velocity.length() >= 0.1:
-		particleSystem1.emitting = true
-		if particleSystem2 != null:
-			particleSystem2.emitting = true
-	else:
-		particleSystem1.emitting = false
-		if particleSystem2 != null:
-			particleSystem2.emitting = false
+	if particleSystem1 and particleSystem2:
+		if linear_velocity.length() >= 0.1:
+			particleSystem1.emitting = true
+			if particleSystem2 != null:
+				particleSystem2.emitting = true
+		else:
+			particleSystem1.emitting = false
+			if particleSystem2 != null:
+				particleSystem2.emitting = false
 
 	#pass
 
@@ -97,11 +82,9 @@ func rotate_around_point(rotate : float):
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.is_pressed():
-			print("Press")
 			offset_held = self.global_position - event.position
 			held = true
 		elif event.is_released():
-			print("Release")
 			held = false
 		viewport.set_input_as_handled();
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
@@ -114,7 +97,6 @@ func _on_input_event(viewport, event, shape_idx):
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.is_released():
-			print("Release")
 			held = false
 
 func _on_mouse_entered():
