751b5efecc0683c23761cc9a69d7069ad423829e
MarkusDennstedt <74238626+MarkusDennstedt@users.noreply.github.com>
2024-01-27 03:31:47 +0100

    reset of own scene (markusTestScene)

--- Scripts/mainMenu.gd
+++ Scripts/mainMenu.gd
@@ -16,4 +16,5 @@ func _on_quit_button_pressed():
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/markusTestScene.tscn")
+	#get_tree().change_scene_to_file("Hier könnte Ihre Szene stehen")
+	pass
