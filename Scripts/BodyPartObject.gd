43385e7be9b81e7036c10700207cbdc97caa810e
Olli1080 <50930405+Olli1080@users.noreply.github.com>
2024-01-27 01:35:28 +0100

    Fix middle of body part snapping to cursor

--- Scripts/BodyPartObject.gd
+++ Scripts/BodyPartObject.gd
@@ -5,6 +5,8 @@ signal released(this : RigidBody2D)
 
 var sprite : Sprite2D
 
+var offset_held = Vector2(0, 0);
+
 var held = false : 
 	get:
 		return held
@@ -31,12 +33,13 @@ func _process(delta):
 
 func _physics_process(delta):
 	if held:
-		global_transform.origin = get_global_mouse_position()
+		global_transform.origin = get_global_mouse_position() + offset_held
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.is_pressed():
 			print("Press")
+			offset_held = self.global_position - event.position
 			held = true
 		elif event.is_released():
 			print("Release")
