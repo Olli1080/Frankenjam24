573abf5e1dfd01c8551d6fa145d996ccd58e683d
Olli1080 <50930405+Olli1080@users.noreply.github.com>
2024-01-27 23:40:44 +0100

    Connect main menu and credits

--- Scenes/credits.gd
+++ Scenes/credits.gd
@@ -3,8 +3,9 @@ extends Control
 @onready var text: Label = get_node("Label")
 @onready var begrenzung: Node2D = get_node("CreditsUpperPart/begrenzung")
 
-signal credits_done
+#signal credits_done
 var done: bool = false
+var timestamp_done = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,9 +14,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if done:
+		if Time.get_ticks_msec() - timestamp_done > 3000:
+			get_tree().change_scene_to_file("res://Scenes/start_menu.tscn")
 		return
 		
-	var offset = delta * 120
+	var offset = delta * 40
 	if text.get_visible_line_count() != text.get_line_count():
 		text.size.y += offset
 		text.position.y -= offset
@@ -23,4 +26,5 @@ func _process(delta):
 		text.position.y -= offset
 		if (begrenzung.global_position.y - (text.position.y + text.size.y) > 0):
 			done = true
-			credits_done.emit()
+			timestamp_done = Time.get_ticks_msec()
+			#credits_done.emit()
