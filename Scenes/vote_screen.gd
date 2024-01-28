193627497feeb1c2138a2b31e8ce8c706e638761
Olli1080 <50930405+Olli1080@users.noreply.github.com>
2024-01-28 02:08:19 +0100

    Add timer to vote screen

--- Scenes/vote_screen.gd
+++ Scenes/vote_screen.gd
@@ -1,14 +1,18 @@
 extends Control
 
-
+@onready var timer: Timer = get_node("Timer")
+@onready var time_label: Label = get_node("Label")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var left = timer.time_left as int
+	var minutes = left / 60
+	var seconds = left - minutes * 60
+	time_label.text = "%02d:%02d" % [minutes, seconds]
 
 
 func _on_cloud_collider_left_clicked():
