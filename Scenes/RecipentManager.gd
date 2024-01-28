f1f8c0af32d3bfd04c4026825d689ff5b6f7cddf
Timm638 <Timm638@gmail.com>
2024-01-28 11:05:32 +0100

    partial commit for voting screen

---
+++ Scenes/RecipentManager.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+@export var next_scene : PackedScene
+
+func store_body_to_morgue():
+	var node : Node2D = self
+	var morgue : TheMorgue = $"/root/TheMorgue"
+	
+	morgue.player_body.pack(node)
+	print("Packed!")
+	
+	get_tree().change_scene_to_packed(next_scene)
